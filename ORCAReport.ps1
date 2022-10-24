5b43309d72f0e4b0a46874b59edb19c6031fa8c2
Carl Lindberg <carllindberg@Carls-MacBook-Pro.local>
2022-10-24 13:51:54 +0200

    removed output script

--- ORCAReport.ps1
+++
@@ -1,8 +0,0 @@
-$outputPath = "C:\Temp\ORCA"
-if (!(Test-Path -Path $outputPath)) {
-    New-Item -Path $outputPath -ItemType Directory
-}
-
-$content = Invoke-ORCA -Output HTML -OutputOptions @{HTML=@{OutputDirectory=$outputPath}} 
-
-$content.result
